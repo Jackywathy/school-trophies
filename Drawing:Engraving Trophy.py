1a85b7bc03a644ba8dc8c84dd9bc586c17fe18c5
Archie Fox <archiefox@Archies-MacBook-Air.local>
2016-05-12 14:49:16 +1000

    asdf

---
+++ Drawing:Engraving Trophy.py
@@ -0,0 +1,27 @@
+from dxfwrite.const import CENTER
+import dxfwrite
+from dxfwrite import DXFEngine as dxf
+
+namedrawing = input("Enter a name for the output file: ")
+if not namedrawing:
+    namedrawing = "doc1.dxf"
+namedrawing += ".dxf"
+drawing = dxf.drawing(namedrawing)
+
+def main(basex, basey, text):
+    drawing.add_layer("dxfwrite")
+    drawing.add_layer("LINES")
+    drawing.add(dxf.line((basex, basey), (basex + 500, basey), color = 1, layer = "LINES", thickness = 0.01))
+    drawing.add(dxf.line((basex + 500, basey), (basex + 500, basey + 100), color = 1, layer = "LINES", thickness = 0.01))
+    drawing.add(dxf.line((basex + 500, basey + 100), (basex, basey + 100), color = 1, layer = "LINES", thickness = 0.01))
+    drawing.add(dxf.line((basex, basey + 100), (basex, basey), color = 1, layer = "LINES", thickness = 0.01))
+    drawing.add_layer("TEXTLAYER")
+    drawing.add(dxf.mtext(text, insert = (basex, basey), height = 50, color = 250, width = 0.01))
+    drawing.save()
+
+names = input("Enter the names you want engraved seperated by a commer: ").split(",")
+
+for name in names:
+    main(0, int(150 * names.index(name)), name)
+
+print("Drawing %s created." % (namedrawing))
