0040242f197d63e45f4fd1a3b1aaa0ef38939773
Archie Fox <ajfox123@gmail.com>
2016-05-12 20:00:59 +1000

    Update for engravings
    Aligns text to be centred horizontally and vertically for any number of engravings

--- Drawing:Engraving Trophy.py
+++ Drawing:Engraving Trophy.py
@@ -16,12 +16,12 @@ def main(basex, basey, text):
     drawing.add(dxf.line((basex + 500, basey + 100), (basex, basey + 100), color = 1, layer = "LINES", thickness = 0.01))
     drawing.add(dxf.line((basex, basey + 100), (basex, basey), color = 1, layer = "LINES", thickness = 0.01))
     drawing.add_layer("TEXTLAYER")
-    drawing.add(dxf.mtext(text, insert = (basex, basey), height = 50, color = 250, width = 0.01))
+    drawing.add(dxf.mtext(text, insert = (basex+250, basey+10), height = 50, color = 250, width = 0.01, valign = dxfwrite.BOTTOM, halign = dxfwrite.CENTER))
     drawing.save()
 
 names = input("Enter the names you want engraved seperated by a commer: ").split(",")
 
 for name in names:
-    main(0, int(150 * names.index(name)), name)
+    main(0, 150 * names.index(name), name.lstrip())
 
 print("Drawing %s created." % (namedrawing))
