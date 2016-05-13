fa7bb1a96b55dfad2b806acb984854cabb1abb39
Jackywathy <jackywathy24@gmail.com>
2016-05-13 15:38:14 +1000

    Working? File for the laser cutting machine\

--- trop.py
+++ trop.py
@@ -1,13 +1,25 @@
 from dxfwrite.const import CENTER
 import dxfwrite
 from dxfwrite import DXFEngine as dxf
-# drawing.add(dxf.line((0, 0), (0,0), color=1, layer='LINES', thickness=0.01))
+
 
 drawing = dxf.drawing("trop.dxf")
 drawing.add_layer('dxfwrite')
 drawing.add_layer('LINES')
-text = dxf.mtext('Line1\nLine2', (0,0), height=0.7,mirror=dxfwrite.MIRROR_X)
-text.layer = 'TEXT'
-text.color = 7
-drawing.add(text)
+drawing.add(dxf.line((0, 0), (90,0), color=1, layer='LINES', thickness=0.00))
+drawing.add(dxf.line((90, 0), (90,225), color=1, layer='LINES', thickness=0.00))
+drawing.add(dxf.line((0, 0), (0,155), color=1, layer='LINES', thickness=0.00))
+drawing.add(dxf.line((90, 225), (0,155), color=1, layer='LINES', thickness=0.00))
+
+
+def text(s,x,y,h,d):
+    text = dxf.mtext(s,(x,y), height=h,style="TIMES_ITALIC",mirror=dxfwrite.MIRROR_X,layer='TEXT')
+    d.add(text)
+
+text('Sydney Boys High School',85,125,5,drawing)
+text('Student Award Scheme',87,115,6,drawing)
+text("The",56,100,10,drawing)
+text("School",67,90,10,drawing)
+text("Trophy",67,77,10,drawing)
+text("SHOVEL",56,38,4,drawing)
 drawing.save()
