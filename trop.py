a4e4995b1b17768c1d454a884ec972177065e78c
Jackywathy <jackywathy24@gmail.com>
2016-05-13 15:56:55 +1000

    new update! 1.9.4

--- trop.py
+++ trop.py
@@ -11,22 +11,28 @@ drawing.add_layer('dxfwrite')
 drawing.add_layer('LINES')
 
 
-def 
-drawing.add(dxf.line((0, 0), (90,0), color=1, layer='LINES', thickness=0.00))
-drawing.add(dxf.line((90, 0), (90,225), color=1, layer='LINES', thickness=0.00))
-drawing.add(dxf.line((0, 0), (0,155), color=1, layer='LINES', thickness=0.00))
-drawing.add(dxf.line((90, 225), (0,155), color=1, layer='LINES', thickness=0.00))
+def create_outline(drawing, x = 0,y = 0):
+    """Creates the outline to the shapes, given x and y offsets"""
+    drawing.add(dxf.line((0 + x, 0 + y), (90 + x, 0 + y), color=1, layer='LINES', thickness=0.00))
+    drawing.add(dxf.line((90 + x, 0 + y), (90 + x, 225 + y) , color=1, layer='LINES', thickness=0.00))
+    drawing.add(dxf.line((0 + x, 0 + y), (0 + x,155 + y), color=1, layer='LINES', thickness=0.00))
+    drawing.add(dxf.line((90 + x, 225 + y), (0 + x,155 + y), color=1, layer='LINES', thickness=0.00))
 
 
-def create_line(text,x,y,height,drawing, style="TIMES_ITALIC"):
+def create_text(text,x,y,height,drawing, style="TIMES_ITALIC"):
     """Create a line in the given drawing"""
     text_var = dxf.mtext(text,(x,y), height=height,style=style,mirror=dxfwrite.MIRROR_X,layer='TEXT')
     drawing.add(text_var)
 
-text('Sydney Boys High School',85,125,5,drawing)
-text('Student Award Scheme',87,115,6,drawing)
-text("The",56,100,10,drawing)
-text("School",67,90,10,drawing)
-text("Trophy",67,77,10,drawing)
-text("SHOVEL",56,38,4,drawing)
+
+
+create_outline(drawing)
+create_text('Sydney Boys High School',85,125,5,drawing)
+create_text('Student Award Scheme',87,115,6,drawing)
+create_text("The",56,100,10,drawing)
+create_text("School",67,90,10,drawing)
+create_text("Trophy",67,77,10,drawing)
+#TODO PUT IN THE RIGHT DIMENSIONS FOR THE awarded to!
+create_text("awarded to", 63, 48, 5, drawing)
+create_text("SHOVEL And JAKK",56,38,5.5,drawing,style = "STANDARD")
 drawing.save()
