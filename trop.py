8cea43183060b820d6cef3e19147f844887f97bb
Jackywathy <jackywathy24@gmail.com>
2016-05-13 15:43:33 +1000

    added some stuff

--- trop.py
+++ trop.py
@@ -3,18 +3,25 @@ import dxfwrite
 from dxfwrite import DXFEngine as dxf
 
 
+offset_left = 0
+
+
 drawing = dxf.drawing("trop.dxf")
 drawing.add_layer('dxfwrite')
 drawing.add_layer('LINES')
+
+
+def 
 drawing.add(dxf.line((0, 0), (90,0), color=1, layer='LINES', thickness=0.00))
 drawing.add(dxf.line((90, 0), (90,225), color=1, layer='LINES', thickness=0.00))
 drawing.add(dxf.line((0, 0), (0,155), color=1, layer='LINES', thickness=0.00))
 drawing.add(dxf.line((90, 225), (0,155), color=1, layer='LINES', thickness=0.00))
 
 
-def text(s,x,y,h,d):
-    text = dxf.mtext(s,(x,y), height=h,style="TIMES_ITALIC",mirror=dxfwrite.MIRROR_X,layer='TEXT')
-    d.add(text)
+def create_line(text,x,y,height,drawing, style="TIMES_ITALIC"):
+    """Create a line in the given drawing"""
+    text_var = dxf.mtext(text,(x,y), height=height,style=style,mirror=dxfwrite.MIRROR_X,layer='TEXT')
+    drawing.add(text_var)
 
 text('Sydney Boys High School',85,125,5,drawing)
 text('Student Award Scheme',87,115,6,drawing)
