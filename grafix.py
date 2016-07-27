f850ea42bcf9941a6b9f300ddf9f962cc9a7046c
Jack Jiang <jackywathy24@gmail.com>
2016-07-27 19:14:04 +1000

    improved grpfix and Notebook!

--- grafix.py
+++ grafix.py
@@ -1,17 +1,20 @@
 from tkinter import *
 from tkinter.filedialog import *
+import tkinter.ttk as ttk
 from platform import system
 import trophy
-
 
 class Application:
     def __init__(self):
         self.arguments = set()
-        self.root = Tk()
-        self.root.bind("<Button-1>", self.callback)
+        self.tk = Tk()
+        self.tk.bind("<Button-1>", self.callback)
+        self.notebook = ttk.Notebook(self.tk)
 
-        self.root.wm_title("Trophy Creator \u0254\u20DD Forge")
-        self.root.iconbitmap('icon.ico')
+        self.photo = PhotoImage(file='icon.ico')
+
+        self.tk.wm_title("Trophy Creator \u0254\u20DD Forge")
+        self.tk.iconbitmap('icon.ico')
 
         # windows only setup
         if system() == "Windows":
@@ -27,38 +30,41 @@ class Application:
         # create grafix
         self.create_frames()
 
-        self.root.update()
+        self.tk.update()
 
 
 
-        self.root.minsize(self.root.winfo_width(), self.root.winfo_height())
+        self.tk.minsize(self.page1.winfo_width(), self.page1.winfo_height())
         mainloop()
 
     def create_frames(self):
-        self.csvFrame = Frame(self.root)
 
+        self.page1 = ttk.Frame()
+        self.page2 = ttk.Frame()
 
+        self.csvFrame = Frame(self.page1)
         self.csvText = Label(self.csvFrame, text="Enter the CSV file")
-
         self.csvBrowse = Button(self.csvFrame,command = self.getFile, text="Browse")
-
         self.csvIn = Entry(self.csvFrame)
         self.csvIn.bind("<Return>", self.getCsvText)
 
-
         self.csvSelected = Label(self.csvFrame, textvariable=self.csvFile)
+        self.csvSeperator = ttk.Separator(self.page1, orient=HORIZONTAL)
 
         self.csvText.grid(row=0,columnspan=2)
         self.csvIn.grid(row=1,column=0)
         self.csvBrowse.grid(row=1,column=1)
         self.csvSelected.grid(row=2,columnspan=2)
 
+
+
+
         ####### Options-
 
-        self.optionFrame = Frame(self.root)
+        self.optionFrame = Frame(self.page1)
         self.optionLabel = Label(self.optionFrame, text="Standard Options")
-        out_temp = IntVar()
-        delete_temp = IntVar()
+        out_temp = BooleanVar()
+        delete_temp = BooleanVar()
         self.outLine = Checkbutton(self.optionFrame,text='Outline', variable=out_temp)
         self.outLine.var = out_temp
 
@@ -73,7 +79,7 @@ class Application:
 
         ### Output location & button
 
-        self.outputFrame = Frame()
+        self.outputFrame = Frame(self.page1)
 
         self.outputEntry = Entry(self.outputFrame,text="Enter Output Location: ")
         self.outputEntry.bind("<Return>", self.getOutputLocation)
@@ -84,13 +90,19 @@ class Application:
         self.outputLabel = Label(self.outputFrame, textvariable=self.output)
         self.outputLabel.grid(row=1)
 
+        # Page1 Frame Grids'
+        self.csvFrame.grid(row=0,column=0,rowspan=2)
+        self.csvSeperator.grid(row=0,column=1,rowspan=2)
+        self.optionFrame.grid(row=0,column=2)
+        self.outputFrame.grid(row=1,column=2)
 
-        self.csvFrame.pack()
+        # Page2 Frame Grid's
+        self.csvFrame.grid(row=0, column=0, rowspan=2)
 
-        self.optionFrame.pack()
 
-        self.outputFrame.pack()
-
+        self.notebook.add(self.page1, text='Trophy', sticky=E+S)
+        self.notebook.add(self.page2, text='Medal', sticky=E+S)
+        self.notebook.pack(anchor=E)
 
 
 
@@ -98,24 +110,15 @@ class Application:
         self.csvFile.set(askopenfilename())
 
     def getCsvText(self, event):
-        self.csvFile.set(self.csvIn.get())
+        self.csvFile.set("../" + self.csvIn.get().split('/')[-1])
 
-    def showAdvancedPressed(self):
-        if self.showAdvanced:
-            self.showAdvanced = False
-            self.showAdvancedButton.grid_forget()
-            self.advancedFrame.grid(row = 10, columnspan=2)
 
-        else:
-            self.showAdvanced = True
-            self.showAdvancedButton.grid(row=10, columnspan=2)
-            self.advancedFrame.grid_forget()
 
     def callback(self,event):
         print(self.outLine.var.get())
 
     def getOutputLocation(self,event):
-        self.output.set(self.outputEntry.get())
+        self.output.set("../" + self.outputEntry.get().split('/')[-1])
 
     def getOutputFile(self):
         self.output.set(askopenfilename())
@@ -124,9 +127,9 @@ class Application:
         trophy.main(list(self.arguments).append(self.csvFile.get()))
 
     def setupWin(self):
-        self.root.wm_iconbitmap(default='icon.ico')
+        self.tk.wm_iconbitmap(default='icon.ico')
     def setupMac(self):
-        self.root.wm_iconbitmap(bitmap='icon.ico')
+        self.tk.wm_iconbitmap(bitmap='icon.ico')
 
 
 
